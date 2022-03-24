617498bc5a9002c48a2c877a4c85b20b59001320
jzironman <jzeffsomera@gmail.com>
2022-03-24 16:15:43 +0800

    'first-change'

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import react from 'react'
+import './header.css';
+
+const Header = () =>{
+    return(
+        <div className='navigator'>
+        Pokedex
+      </div>
+    )
+}
+
+export default Header;
---
+++ src/pokemon.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+
+const Pokemon = () =>{
+    return(
+        <div className='navigator'>
+        Pokemon
+      </div>
+    )
+}
+
+export default Pokemon;
